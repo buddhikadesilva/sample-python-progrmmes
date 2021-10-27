dd50125243d49a33139473309b799da3aa57856a
Vinuri Ranawaka <73119621+vinuriR@users.noreply.github.com>
2021-10-27 09:29:07 +0530

    Update and rename 1.py to largest number.py

---
+++ largest number.py
@@ -0,0 +1,13 @@
+dfsfsd
+a=int(input('Enter number 1:'))
+b=int(input('Enter number 2:'))
+c=int(input('Enter number 3:'))
+if a>b:
+    if a>c:
+        print(a)
+    else:
+        print(c)
+elif b>c:
+    print(b)
+else:
+    print(c)
